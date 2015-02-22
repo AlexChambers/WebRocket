47e09c6a5b1796d843f02168c2911a7b9f3d029d
Alex Chambers <achambe4@asu.edu>
2015-02-21 21:15:50 -0700

    .
    .

--- script/script.js
+++ script/script.js
@@ -1,20 +1,25 @@
 function Test() {
+    "use strict";
     window.open("https://www.github.com");
     window.open("https://www.reddit.com");
 }
 
 function School() {
-    /*ASU*/   window.open("https://www.asu.edu");
-    /*NYU*/   window.open("http://www.nyu.edu/");
-    /*UCB*/   window.open("http://www.berkeley.edu/");
+    "use strict";
+    window.open("https://www.asu.edu");        /*ASU*/
+    window.open("http://www.nyu.edu/");        /*NYU*/
+    window.open("http://www.berkeley.edu/");   /*UCB*/
 }
 
 function Research() {
-    /*HTML*/   window.open("http://en.wikipedia.org/wiki/HTML");
-    /* CSS*/   window.open("http://en.wikipedia.org/wiki/Cascading_Style_Sheets");
-    /*  JS*/   window.open("http://en.wikipedia.org/wiki/JavaScript");
+    "use strict";
+    window.open("http://en.wikipedia.org/wiki/HTML");                  /*HTML*/
+    window.open("http://en.wikipedia.org/wiki/Cascading_Style_Sheets");/*CSS*/
+    window.open("http://en.wikipedia.org/wiki/JavaScript");            /*JavaScript*/
 }
 
 function Settings() {
+    "use strict";
+    var alert;
     alert("\"Settings\" is still under development.");
 }
