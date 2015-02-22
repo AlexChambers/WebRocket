46c7ee28871b82e589190e32fea43985ee61f759
Alex Chambers <achambe4@asu.edu>
2015-02-21 18:50:23 -0700

    Changes folder location and renamed functions
    * Changed location of the URLs for "School"
    ** page is still static: predetermined URL locations **

--- script/script.js
+++ script/script.js
@@ -1,34 +1,20 @@
-function Test()
-{
+function Test() {
     window.open("https://www.github.com");
     window.open("https://www.reddit.com");
 }
 
-function SchoolButton()
-{
-    /*CSE340*/   window.open("https://myasucourses.asu.edu/webapps/asu-courseRedirect-bb_bb60/redirect/2015Spring-T-CSE340-11830-17038");
-/*CSE445*/   window.open("https://myasucourses.asu.edu/webapps/asu-courseRedirect-bb_bb60/redirect/2015Spring-T-CSE445-CSE598-11845-16399");
-/*CSE460*/   window.open("https://myasucourses.asu.edu/webapps/asu-courseRedirect-bb_bb60/redirect/2015Spring-T-CSE460-CSE598-12441-16401");
-/*CSE463*/   window.open("https://myasucourses.asu.edu/webapps/asu-courseRedirect-bb_bb60/redirect/2015Spring-T-CSE463-17010");
-/*Piazza*/   window.open("https://piazza.com/class/i4lkyncm59i321");
-    //window.open("http://alexchambers.github.io");
-    window.open("https://www.mindmeister.com");
-    window.open("https://gmail.google.com");
+function School() {
+    /*ASU*/   window.open("https://www.asu.edu");
+    /*NYU*/   window.open("http://www.nyu.edu/");
+    /*UCB*/   window.open("http://www.berkeley.edu/");
 }
 
-function SearchEngineButton()
-{
-    window.open("https://www.yahoo.com");
-    window.open("http://www.msn.com");
-    window.open("https://www.google.com");
+function Research() {
+    /*HTML*/   window.open("http://en.wikipedia.org/wiki/HTML");
+    /* CSS*/   window.open("http://en.wikipedia.org/wiki/Cascading_Style_Sheets");
+    /*  JS*/   window.open("http://en.wikipedia.org/wiki/JavaScript");
 }
 
-function WebDevButton()
-{
-    
-}
-
-function GroupButton()
-{
-
+function Settings() {
+    alert("\"Settings\" is still under development.");
 }
